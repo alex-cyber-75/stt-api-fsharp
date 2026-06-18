93c41d605c5c81219cdb7eee7075e654dab7e1ad
Alex-Cyber <alex.github.75@gmail.com>
2026-06-18 20:44:45 +0000

    Add stub transcription test with hello_world.mp3

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -1,7 +1,13 @@
 namespace Tests
 open NUnit.Framework
 open STTApi
+open System.Threading.Tasks
 
 [Test]
-let ``Hello world test`` () =
-    Assert.IsTrue(true)
+let ``Transcriber returns known phrase`` () : Task =
+    async {
+        let mp3Path = __SOURCE_DIRECTORY__ + "/hello_world.mp3"
+        let dummyBytes = System.IO.File.ReadAllBytes(mp3Path)
+        let! result = Transcriber.transcribe dummyBytes
+        Assert.AreEqual("Hello world", result)
+    } |> Async.StartAsTask
